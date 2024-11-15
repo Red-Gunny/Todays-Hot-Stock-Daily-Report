19180f5e8f3acad31c2ea3bc6b5fcfbf145eadde
hong-geon-ui <nehepkldi@naver.com>
2024-11-15 18:19:37 +0900

    Test : properties 바인딩 확인 테스트 수행

--- src/main/kotlin/geonui/Todays_Hot_Stock_Daily_Report/dto/body/AuthBody.kt
+++ src/main/kotlin/geonui/Todays_Hot_Stock_Daily_Report/dto/body/AuthBody.kt
@@ -3,16 +3,11 @@ package geonui.Todays_Hot_Stock_Daily_Report.dto.body
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
-/**
- * Kotlin에서는 클래스의 생성자 파라미터에 val 또는 var 키워드를 붙이면
- * 해당 파라미터가 클래스의 프로퍼티로 선언된다.
- *
- * val 빼면 클래스 프로퍼티로 선언되지 않는다. 클래스 내 다른 메소드에서는 사용 불가.
- */
+
 @Component
 data class AuthBody(
     @Value("\${spring.appkey}") val appkey: String,
     @Value("\${spring.appsecret}") val appsecret: String,
 ) {
-    private val grant_type: String = "client_credentials"
+    val grant_type: String = "client_credentials"
 }
--- src/test/kotlin/geonui/Todays_Hot_Stock_Daily_Report/dto/AuthBodyTest.kt
+++ src/test/kotlin/geonui/Todays_Hot_Stock_Daily_Report/dto/AuthBodyTest.kt
@@ -1,30 +1,30 @@
 package geonui.Todays_Hot_Stock_Daily_Report.dto
 
 import geonui.Todays_Hot_Stock_Daily_Report.dto.body.AuthBody
+import org.assertj.core.api.Assertions
+
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.server.LocalServerPort
-import org.springframework.context.annotation.Import
+import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
-import kotlin.test.assertNotNull
 
 
-@ContextConfiguration(classes = [AuthBody::class])
+@SpringBootTest(classes = [AuthBody::class])    // 클래스,함수,프로퍼티 참조
 @ActiveProfiles("dev")
 class AuthBodyTest {
-
 
     @Autowired
     private lateinit var authBody: AuthBody
 
+    @Value("\${spring.appkey}")
+    private lateinit var appKey: String
+
     @Test
     fun AuthBody_Binding_Success() {
-        assertNotNull(authBody.appkey)
-
-
+        Assertions.assertThat(authBody.appkey).isEqualTo(appKey)
+        Assertions.assertThat(authBody.grant_type).isEqualTo("client_credentials")
     }
 
 }
